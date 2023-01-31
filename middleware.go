6512fbd10e8767d5946cefba2c77c891a2528590
Wilfredo Guardado <wilfredoguardadog@outlook.com>
2023-01-31 13:58:33 -0600

    Adding Logging middleware

--- middleware.go
+++ middleware.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 )
 
 func CheckAut() Middleware {
@@ -15,6 +17,18 @@ func CheckAut() Middleware {
 			} else {
 				return
 			}
+		}
+	}
+}
+
+func Logging() Middleware {
+	return func(f http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			defer func() {
+				log.Println(r.URL.Path, time.Since(start))
+			}()
+			f(w, r)
 		}
 	}
 }
