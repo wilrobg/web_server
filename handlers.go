3ca2f6aeb0edbf431ae68d08bb8a104a7a32d63f
Wilfredo Guardado <wilfredoguardadog@outlook.com>
2023-01-31 13:06:51 -0600

    Handle api endpoint

--- handlers.go
+++ handlers.go
@@ -8,3 +8,7 @@ import (
 func HandleRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World!")
 }
+
+func HandleHome(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is an API endpoint")
+}
